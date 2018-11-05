e19bc3c9a610bd860032562aa838a069bf55d1f8
Romain Cendre <romain.cendre@gmail.com>
2018-11-05 10:06:29 +0100

    Adding transformation file to implements own transforms.

---
+++ core/transforms.py
@@ -0,0 +1,97 @@
+from pywt import dwt
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+class DWTTransform(BaseEstimator, TransformerMixin):
+    """Class that manage a DWT Transform
+
+     This class is made the same way as sklearn transform to be fit in a Pipe
+
+     Attributes:
+         mode (:obj:'str'): A mode for DWT extraction.
+
+     """
+
+    def __init__(self, mode='db1', segment_length=-1):
+        """Make an initialisation of DWTTransform object.
+
+        Take a string that represent extraction mode, default use 'db1'
+
+        Args:
+             mode (:obj:'str'): The mode as string.
+        """
+        self.mode = mode
+        self.segment_length = segment_length
+
+    def fit(self, x, y=None):
+        """
+        This should fit this transformer, but DWT doesn't need to fit to train data
+
+        Args:
+             x (:obj): Not used.
+             y (:obj): Not used.
+        """
+        return self
+
+    def transform(self, x):
+        """
+        This method is the main part of this transformer.
+        Return a wavelet transform, as specified mode.
+
+        Args:
+             x (:obj): Not used.
+        """
+        (cA, _) = dwt(x, self.mode)
+        return cA
+        # if x.ndim == 2:
+        #     data_length = x.shape[1]
+        # else:
+        #     data_length = len(x)
+        #
+        # if self.segment_length == -1:
+        #     length = data_length
+        # else:
+        #     length = self.segment_length
+        #
+        # chunks = [x[:, i:i+length] for i in range(0, data_length, length)]
+        #
+        # res = None
+        # for i in range(len(chunks)):
+        #     (cA, _) = dwt(chunks[i], self.mode)
+        #     if res is None:
+        #         res = cA
+        #     else:
+        #         res = concatenate((res, cA), axis=1)
+        # return res
+
+
+class PLSTransform(PLSRegression):
+
+    def transform(self, x, y=None, copy=True):
+        """
+        This method is the main part of this transformer.
+        Return a wavelet transform, as specified mode.
+
+        Args:
+             x (:obj): Not used.
+             y (:obj): Not used.
+             copy (:obj): Not used.
+        """
+        return super(PLSRegression, self).transform(x)
+
+
+class LDATransform(LinearDiscriminantAnalysis):
+
+    def transform(self, x, y=None, copy=True):
+        """
+        This method is the main part of this transformer.
+        Return a wavelet transform, as specified mode.
+
+        Args:
+             x (:obj): Not used.
+             y (:obj): Not used.
+             copy (:obj): Not used.
+        """
+        return super(LinearDiscriminantAnalysis, self).transform(x)
