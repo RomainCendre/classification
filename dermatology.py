95f88f74d5444b65b65be0c15e9ada5dce876c93
Romain Cendre <romain.cendre@gmail.com>
2019-01-08 16:05:23 +0100

    Adding labels extraction

--- dermatology.py
+++ dermatology.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import GroupKFold
 from IO.dermatology import Reader, DataManager
 from core.classification import ClassifierDeep
 from tools.limitations import Parameters
+from tools.tensorboard import TensorBoardTool
 
 outer_cv = GroupKFold(n_splits=5)
 
@@ -20,24 +21,26 @@ if __name__ == '__main__':
     # Prepare data
     origin_folder = normpath('{home}/Data/Skin/Saint_Etienne/Original'.format(home=home_path))
     patient_folder = normpath('{home}/Data/Skin/Saint_Etienne/Patients'.format(home=home_path))
-    DataManager(origin_folder).launch_converter(patient_folder)
+    # DataManager(origin_folder).launch_converter(patient_folder)
 
     # Configure GPU consumption
     Parameters.set_gpu(percent_gpu=0.5)
 
     # Load data references
     dataset = Reader().scan_folder(patient_folder)
-    datas = dataset.get_data(filter_by={'Modality': 'Microscopy'})
-
+    paths = dataset.get_data(filter_by={'Modality': 'Microscopy',
+                                        'Label': ['LM', 'Normal']})
+    labels = dataset.get_meta(meta='Label', filter_by={'Modality': 'Microscopy',
+                                        'Label': ['LM', 'Normal']})
     # Adding process to watch our training process
     current_time = strftime('%Y_%m_%d_%H_%M_%S', gmtime(time()))
     work_dir = normpath('{output_dir}/Graph/{time}'.format(output_dir=output_dir, time=current_time))
     makedirs(work_dir)
 
     # Tensorboard tool launch
-    # tb_tool = TensorBoardTool(work_dir)
-    # tb_tool.write_batch()
-    # tb_tool.run()
+    tb_tool = TensorBoardTool(work_dir)
+    tb_tool.write_batch()
+    tb_tool.run()
 
     results = []
     classifier = ClassifierDeep(outer_cv=outer_cv, work_dir=work_dir)
