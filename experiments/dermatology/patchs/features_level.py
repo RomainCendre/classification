d1ee22c60fac95fd2ffb9e578be2e9c19109c1af
Romain Cendre <romain.cendre@gmail.com>
2019-03-27 08:29:38 +0100

    Update feature level patch classification.

--- experiments/dermatology/patchs/features_level.py
+++ experiments/dermatology/patchs/features_level.py
@@ -3,10 +3,12 @@ from copy import deepcopy
 from os import makedirs, startfile
 from os.path import normpath, exists, expanduser, splitext, basename, join
 from sklearn.model_selection import StratifiedKFold, GroupKFold
+from sklearn.preprocessing import LabelEncoder
+
 from experiments.processes import Process
-from toolbox.IO.datasets import Dataset
+from toolbox.IO.datasets import Dataset, DefinedSettings
 from toolbox.core.models import Transforms, Classifiers
-from toolbox.core.structures import Settings
+from toolbox.core.transforms import OrderedEncoder
 from toolbox.tools.limitations import Parameters
 
 if __name__ == "__main__":
@@ -15,10 +17,10 @@ if __name__ == "__main__":
     home_path = expanduser('~')
     validation = StratifiedKFold(n_splits=5, shuffle=True)
     test = validation  # GroupKFold(n_splits=5)
-    settings = Settings({'labels_colors': dict(Malignant=(1, 0, 0), Benign=(0.5, 0.5, 0), Normal=(0, 1, 0))})
+    settings = DefinedSettings.get_default_dermatology()
 
     # Output folders
-    output_folder = normpath('{home}/Results/Dermatology/SVM/{filename}/'.format(home=home_path, filename=filename))
+    output_folder = normpath('{home}/Results/Dermatology/feature_level/{filename}/'.format(home=home_path, filename=filename))
     if not exists(output_folder):
         makedirs(output_folder)
 
@@ -34,6 +36,12 @@ if __name__ == "__main__":
     if not exists(projection_folder):
         makedirs(projection_folder)
 
+    # Filters
+    filters = [('Results_All', {'Label': ['Normal', 'Benign', 'Malignant']}),
+               ('Results_NvsM', {'Label': ['Normal', 'Malignant']}),
+               ('Results_NvsB', {'Label': ['Normal', 'Benign']}),
+               ('Results_BvsM', {'Label': ['Benign', 'Malignant']})]
+
     # Configure GPU consumption
     Parameters.set_gpu(percent_gpu=0.5)
 
@@ -42,8 +50,6 @@ if __name__ == "__main__":
 
     # Methods
     methods = [('Haralick', Transforms.get_haralick(mean=False)),
-               ('HaralickMean', Transforms.get_haralick(mean=True)),
-               ('Wavelet', Transforms.get_image_dwt()),
                ('KerasAverage', Transforms.get_keras_extractor(pooling='avg')),
                ('KerasMaximum', Transforms.get_keras_extractor(pooling='max'))]
 
@@ -54,12 +60,18 @@ if __name__ == "__main__":
     process = Process()
     process.begin(inner_cv=validation, outer_cv=test, n_jobs=2, settings=settings)
 
-    for method in methods:
+    combinations = list(itertools.product(filters, methods))
+    for combination in combinations:
+        filter, method = combination
+
         working_input = deepcopy(input)
-        name = '{method}'.format(method=method[0])
+        working_input.set_filters(filter[1])
+        working_input.set_encoders({'label': OrderedEncoder().fit(filter[1]['Label']),
+                                    'groups': LabelEncoder()})
+        name = '{filter}_{method}'.format(filter=filter[0], method=method[0])
         process.checkpoint_step(inputs=working_input, model=method[1], folder=features_folder,
                                 projection_folder=projection_folder, projection_name=name)
-        working_input.patch_method()
+        working_input.collapse(reference_tag='Reference', data_tag='Data', flatten=True)
         process.end(inputs=working_input, model=model, output_folder=output_folder, name=name)
 
     # Open result folder
