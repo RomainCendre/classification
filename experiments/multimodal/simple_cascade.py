8d63bd38c572b69ee255d9a0ffdcadda1767553b
Romain Cendre <romain.cendre@gmail.com>
2019-04-18 09:25:29 +0200

    Add some package files and prepare multimodal.

---
+++ experiments/multimodal/simple_cascade.py
@@ -0,0 +1,57 @@
+import itertools
+from os import makedirs, startfile
+from os.path import normpath, exists, expanduser, splitext, basename, join
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+
+from experiments.processes import Process
+from toolbox.IO.datasets import Dataset, DefinedSettings
+from toolbox.core.builtin_models import Transforms, Classifiers
+from toolbox.core.transforms import OrderedEncoder
+from toolbox.tools.limitations import Parameters
+
+if __name__ == "__main__":
+
+    # Configure GPU consumption
+    Parameters.set_gpu(percent_gpu=0.5)
+
+    # Parameters
+    filename = splitext(basename(__file__))[0]
+    home_path = expanduser('~')
+    validation = StratifiedKFold(n_splits=5)
+    test = validation
+    settings = DefinedSettings.get_default_dermatology()
+
+    # Output folders
+    output_folder = normpath('{home}/Results/MultiModal/{filename}/'.format(home=home_path, filename=filename))
+    if not exists(output_folder):
+        makedirs(output_folder)
+
+    features_folder = join(output_folder, 'Features')
+    if not exists(features_folder):
+        makedirs(features_folder)
+
+    projection_folder = join(output_folder, 'Projection')
+    if not exists(projection_folder):
+        makedirs(projection_folder)
+
+    # Filters
+    filters = {'Label': ['Normal', 'Benign', 'Malignant']}
+
+    # Input patch
+    inputs = Dataset.full_images()
+
+    # Methods
+    methods = Transforms.get_keras_extractor(pooling='max')
+
+    # Models
+    pipe, parameters = Classifiers.get_linear_svm()
+
+
+    inputs.name = 'Multimodal'
+    inputs.set_filters(filters)
+    inputs.set_encoders({'label': OrderedEncoder().fit(filters['Label']),
+                         'groups': LabelEncoder()})
+
+    # Open result folder
+    startfile(output_folder)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='ClassificationTools',
+    version='0.1dev',
+    packages=['toolbox',],
+    long_description=open('README.md').read(),
+)
