9bc8fc90f6de6d9e4bdb77a0d0f89adf5feb8398
Romain Cendre <romain.cendre@gmail.com>
2019-05-06 16:20:52 +0200

    Update simple classification experiment.

--- experiments/dermatology/simple/simple.py
+++ experiments/dermatology/simple/simple.py
@@ -31,48 +31,49 @@ if __name__ == "__main__":
     if not exists(features_folder):
         makedirs(features_folder)
 
-    projection_folder = join(output_folder, 'Projection')
-    if not exists(projection_folder):
-        makedirs(projection_folder)
-
     # Statistics expected
     statistics = ['Sex', 'Diagnosis', 'Binary_Diagnosis', 'Area', 'Label']
 
     # Filters
-    filters = [('All', {'Label': ['Normal', 'Benign', 'Malignant']}, {}),
-               ('NvsP', {'Label': ['Normal', 'Pathology']}, {'Label': (['Benign', 'Malignant'], 'Pathology')}),
-               ('MvsR', {'Label': ['Malignant', 'Rest']}, {'Label': (['Normal', 'Benign'], 'Rest')})]
+    filters = [('All', {'Label': ['Normal', 'Benign', 'Malignant'], 'Diagnosis': ['LM/LMM', 'SL', 'AL']}, {}),
+               ('NvsP', {'Label': ['Normal', 'Pathology'], 'Diagnosis': ['LM/LMM', 'SL', 'AL']}, {'Label': (['Benign', 'Malignant'], 'Pathology')}),
+               ('MvsR', {'Label': ['Malignant', 'Rest'], 'Diagnosis': ['LM/LMM', 'SL', 'AL']}, {'Label': (['Normal', 'Benign'], 'Rest')})]
+
+    # Image filters
+    scales = [('Full', {'Type': 'Full'}), ('Thumbnails', {'Type': 'Patch'})]
 
     # Input patch
-    inputs = [('Thumbnails', Dataset.thumbnails()),
-              ('Full', Dataset.full_images())]
+    inputs = Dataset.images()
 
     # Methods
-    methods = [('Haralick', Transforms.get_haralick(mean=False)),
+    methods = [('Wavelet', Transforms.get_image_dwt()),
+               ('Haralick', Transforms.get_haralick(mean=False)),
                ('KerasAverage', Transforms.get_keras_extractor(pooling='avg')),
                ('KerasMaximum', Transforms.get_keras_extractor(pooling='max'))]
 
     # Models
     models = [('Svm', Classifiers.get_linear_svm())]
-              # ('SvmPca', Classifiers.get_linear_svm(reduce=[20, 40, 100]))]
 
     # Parameters combinations
-    combinations = list(itertools.product(inputs, methods, models))
+    combinations = list(itertools.product(methods, models))
 
-    # Thumbnails
+    # Browse combinations
     for filter_name, filter_datas, filter_groups in filters:
 
         process = Process(output_folder=output_folder, name=filter_name, settings=settings, stats_keys=statistics)
         process.begin(inner_cv=validation, outer_cv=test, n_jobs=4)
 
-        for input, method, model in combinations:
-            copy_input = input[1].copy_and_change(filter_groups)
-            copy_input.name = '{input}_{method}_{model}'.format(input=input[0], method=method[0], model=model[0])
-            copy_input.set_filters(filter_datas)
-            copy_input.set_encoders({'label': OrderedEncoder().fit(filter_datas['Label']),
-                                   'groups': LabelEncoder()})
-            process.checkpoint_step(inputs=copy_input, model=method[1], folder=features_folder)
-            process.evaluate_step(inputs=copy_input, model=model[1])
+        for method, model in combinations:
+
+            for scale in scales:
+                copy_input = inputs.copy_and_change(filter_groups)
+                copy_input.name = '{input}_{method}_{model}'.format(input=inputs, method=method[0], model=model[0])
+
+                filter_datas.update(scale)
+                copy_input.set_filters(filter_datas)
+                copy_input.set_encoders({'label': OrderedEncoder().fit(filter_datas['Label']), 'groups': LabelEncoder()})
+                process.checkpoint_step(inputs=copy_input, model=method[1], folder=features_folder)
+                process.evaluate_step(inputs=copy_input, model=model[1])
         process.end()
 
     # Open result folder
