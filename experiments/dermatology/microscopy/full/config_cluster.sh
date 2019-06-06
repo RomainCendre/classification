28a510fc3086704d16a4cda1087d0ec784ea3b55
Romain Cendre <romain.cendre@gmail.com>
2019-06-06 16:41:52 +0200

    Update cluster files.

--- experiments/dermatology/microscopy/full/config_cluster.sh
+++ experiments/dermatology/microscopy/full/config_cluster.sh
@@ -5,8 +5,5 @@ ln -s /work/le2i/rc621381/conda/tmp ~/.conda/pkgs
 cd /work/le2i/rc621381/
 module load pytorch/19.01-nvidia-singularity
 singularity-exec-pytorch conda env create -p /work/le2i/rc621381/conda/envs/GPU -f environmentGPU.yml
-singularity-exec-pytorch conda activate /work/le2i/rc621381/conda/envs/GPU
-
-~/my-envs/GPU/bin/python classification/experiments/dermatology/microscopy/full/fine_tune.py
 
 
