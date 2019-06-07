3f29ae45ed53fa046511e5450570119fda6dbcad
Romain Cendre <romain.cendre@gmail.com>
2019-06-07 15:34:40 +0200

    Update cluster configuration script.

--- experiments/cluster/config_cluster.sh
+++ experiments/cluster/config_cluster.sh
@@ -1,12 +1,12 @@
 # Create working directory
 mkdir /work/le2i/rc621381/Data
-mkdir /work/le2i/rc621381/Features
+mkdir /work/le2i/rc621381/.research
 mkdir /work/le2i/rc621381/Results
 mkdir /work/le2i/rc621381/XDG
 mkdir /work/le2i/rc621381/conda/envs
 # Now create link to these folders
 ln -s /work/le2i/rc621381/Data ~/Data
-ln -s /work/le2i/rc621381/Features ~/Features
+ln -s /work/le2i/rc621381/.research ~/.research
 ln -s /work/le2i/rc621381/Results ~/Results
 ln -s /work/le2i/rc621381/conda/envs ~/my-envs
 ln -s /work/le2i/rc621381/conda/tmp ~/.conda/pkgs
