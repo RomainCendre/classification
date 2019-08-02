4d54fd12454dca73f4412678f353e3e510868671
Romain Cendre <romain.cendre@gmail.com>
2019-08-02 15:53:13 +0200

    Update main file.

--- toolbox/gui_tools/patchifier/main.py
+++ toolbox/gui_tools/patchifier/main.py
@@ -1,21 +1,14 @@
 import sys
-from os import makedirs
-from os.path import expanduser, exists, normpath
 from pathlib import Path
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
-from toolbox.core.parameters import DermatologyDataset, BuiltInSettings
+from toolbox.core.parameters import BuiltInSettings
 from toolbox.gui_tools.patchifier.sources.gui import QPatchExtractor
 
 if __name__ == '__main__':
 
-    home_path = expanduser('~')
-    patch_folder = '{home}/Data/Skin/Patches'.format(home=home_path)
-    patch_folder = normpath(patch_folder)
-    if not exists(patch_folder):
-        makedirs(patch_folder)
     # Create the QApplication.
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon('./images/icon.png'))
