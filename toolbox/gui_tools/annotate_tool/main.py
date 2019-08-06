65c10268a4eaf4242bc037991a0f9089d2e0b022
Romain Cendre <romain.cendre@gmail.com>
2019-08-06 10:40:56 +0200

    Some refactoring.

--- toolbox/gui_tools/annotate_tool/main.py
+++ toolbox/gui_tools/annotate_tool/main.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from toolbox.core.parameters import BuiltInSettings
-from toolbox.gui_tools.patchifier.sources.gui import QPatchExtractor
+from toolbox.gui_tools.annotate_tool.sources.gui import QPatchExtractor
 
 if __name__ == '__main__':
 
