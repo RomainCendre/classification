2340cc14eb00dc0815f3809cc3dad28e2c590779
Romain Cendre <romain.cendre@gmail.com>
2019-09-13 08:47:01 +0200

    Adding compute methods.

--- experiments/dermatology/microscopy/a_deep.py
+++ experiments/dermatology/microscopy/a_deep.py
@@ -165,11 +165,11 @@ if __name__ == "__main__":
         # Transfer Learning
         output = output_folder / 'transfer' / image_type
         output.mkdir(parents=True, exist_ok=True)
-        # transfer_learning(inputs, output)
+        transfer_learning(inputs, output)
         # Fine Learning
         output = output_folder / 'fine' / image_type
         output.mkdir(parents=True, exist_ok=True)
-        # fine_tune(inputs, output)
+        fine_tune(inputs, output)
 
     # Open result folder
     webbrowser.open(output_folder.as_uri())
