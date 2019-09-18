aba3a3c9c275e5bd945797ddc0b16d86dafa9f1e
Romain Cendre <romain.cendre@gmail.com>
2019-09-18 12:03:36 +0200

    Adding some spyder local configuration for GUI.

--- experiments/settings/spyder_server/get_configurations.bat
+++ experiments/settings/spyder_server/get_configurations.bat
@@ -1,1 +1,1 @@
-pscp -r rcendre@calcul.6306-1.rp.u-bourgogne.fr:/home/rcendre/servers %userprofile%.local/share/jupyter/runtime/
+pscp -r rcendre@calcul.6306-1.rp.u-bourgogne.fr:/home/rcendre/servers/* %userprofile%/AppData\Roaming\jupyter\runtime
---
+++ experiments/settings/spyder_server/gui.bat
@@ -0,0 +1,3 @@
+conda create --name Spyder python=3.7
+conda install spyder
+conda install paramiko
