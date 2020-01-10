26ef82b58371d17d6c5634e0eb795ab3e7423478
Romain Cendre <romain.cendre@gmail.com>
2020-01-10 18:04:17 +0100

    Fix score tool

--- guis/demo_food/main.py
+++ guis/demo_food/main.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from PyQt5.QtGui import QIcon, QPalette
 from PyQt5.QtWidgets import QApplication, QFileDialog
 from guis.demo_food.sources.gui import QDemo
-from toolbox.classification.common import IO
 
 if __name__ == '__main__':
 
@@ -14,13 +13,11 @@ if __name__ == '__main__':
     # Load resources
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon(str(icon_path)))
-    print(app.palette().color(QPalette.Text).name())
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    print(app.palette().color(QPalette.Text).name())
 
     # Launch gui
     viewer = QDemo()
-    # viewer.load_data()
+    viewer.load_data()
     viewer.show()
 
     sys.exit(app.exec_())
