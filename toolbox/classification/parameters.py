ec85f34992a8b48c28e87a893e51f7a8e1234554
Romain Cendre <romain.cendre@gmail.com>
2020-01-16 18:28:29 +0100

    Solve configproto

--- toolbox/classification/parameters.py
+++ toolbox/classification/parameters.py
@@ -362,8 +362,14 @@ class LocalParameters:
             session = tf.Session(config=config)
             set_session(session)
         else:
-            config = tf.compat.v1.ConfigProto()
-            config.gpu_options.allow_growth = allow_growth
-            config.gpu_options.per_process_gpu_memory_fraction = percent_gpu
-            session = tf.compat.v1.Session(config=config)
-            tf.compat.v1.keras.backend.set_session(session)
+            gpus = tf.config.experimental.list_physical_devices('GPU')
+            if gpus:
+                try:
+                    # Restrict TensorFlow to only use the fourth GPU
+                    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+
+                    # Currently, memory growth needs to be the same across GPUs
+                    for gpu in gpus:
+                        tf.config.experimental.set_memory_growth(gpu, True)
+                    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
