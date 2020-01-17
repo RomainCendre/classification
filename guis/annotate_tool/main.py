73ee0001903e7c3035e5973ee7b2ea344aecc4ed
Romain Cendre <romain.cendre@gmail.com>
2020-01-17 14:27:46 +0100

    Update annotation tool

--- guis/annotate_tool/main.py
+++ guis/annotate_tool/main.py
@@ -1,4 +1,5 @@
 import sys
+import qdarkstyle
 from pathlib import Path
 
 from PyQt5.QtGui import QIcon
@@ -9,13 +10,18 @@ from toolbox.classification.parameters import Settings
 if __name__ == '__main__':
 
     # Create the QApplication.
-    app = QApplication(sys.argv)
     icon_path = Path(__file__).parent/'images'/'icon.png'
+
+    # Load resources
+    app = QApplication(sys.argv)
     app.setWindowIcon(QIcon(str(icon_path)))
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     pathologies = ['Normal', 'Benign', 'Malignant', 'Draw']
     home_path = Path().home()
     root_data = home_path/'Data/Skin/'
     input_files = ['Elisa.csv', 'JeanLuc.csv']
+
+    # Launch gui
     viewer = QPatchExtractor(root_data, input_files, pathologies, settings=Settings.get_default_dermatology())
 
     # Show the viewer and run the application.
