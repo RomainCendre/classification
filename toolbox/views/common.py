bec5f63287595da50a6a7624975ca9776cbadd5c
Romain Cendre <romain.cendre@gmail.com>
2020-05-19 19:30:30 +0200

    Adding ellipsis.

--- toolbox/views/common.py
+++ toolbox/views/common.py
@@ -6,8 +6,9 @@ from collections import Counter
 from jinja2 import Environment, ChoiceLoader, FileSystemLoader
 from matplotlib.colors import ListedColormap
 from pandas.io.formats.style import Styler
-from matplotlib import pyplot
+from matplotlib import pyplot, transforms
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.patches import Ellipse
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.metrics import auc, roc_curve, classification_report
@@ -15,6 +16,61 @@ from toolbox.classification.common import Tools
 
 
 class Views:
+    def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+        """
+        Create a plot of the covariance confidence ellipse of `x` and `y`
+
+        Parameters
+        ----------
+        x, y : array_like, shape (n, )
+            Input data.
+
+        ax : matplotlib.axes.Axes
+            The axes object to draw the ellipse into.
+
+        n_std : float
+            The number of standard deviations to determine the ellipse's radiuses.
+
+        Returns
+        -------
+        matplotlib.patches.Ellipse
+
+        Other parameters
+        ----------------
+        kwargs : `~matplotlib.patches.Patch` properties
+        """
+        if x.size != y.size:
+            raise ValueError("x and y must be the same size")
+
+        cov = np.cov(x, y)
+        pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+        # Using a special case to obtain the eigenvalues of this
+        # two-dimensionl dataset.
+        ell_radius_x = np.sqrt(1 + pearson)
+        ell_radius_y = np.sqrt(1 - pearson)
+        ellipse = Ellipse((0, 0),
+                          width=ell_radius_x * 2,
+                          height=ell_radius_y * 2,
+                          facecolor=facecolor,
+                          **kwargs)
+
+        # Calculating the stdandard deviation of x from
+        # the squareroot of the variance and multiplying
+        # with the given number of standard deviations.
+        scale_x = np.sqrt(cov[0, 0]) * n_std
+        mean_x = np.mean(x)
+
+        # calculating the stdandard deviation of y ...
+        scale_y = np.sqrt(cov[1, 1]) * n_std
+        mean_y = np.mean(y)
+
+        transf = transforms.Affine2D() \
+            .rotate_deg(45) \
+            .scale(scale_x, scale_y) \
+            .translate(mean_x, mean_y)
+
+        ellipse.set_transform(transf + ax.transData)
+        return ax.add_patch(ellipse)
 
     @staticmethod
     def details(inputs, tags):
@@ -153,17 +209,22 @@ class Views:
         projected = method.fit_transform(np.array(inputs[tags['datum']].tolist()))
 
         figure = pyplot.figure()
+        axis = figure.add_subplot(111)
         for label in ulabels:
             color = np.expand_dims(np.array(settings.get_color(label)), axis=0)
             pyplot.scatter(projected[labels == label, 0], projected[labels == label, 1],
                            c=color, alpha=0.5, label=label, edgecolor='none')
-
+        # Centroids & Ellipsis
         for label in ulabels:
-            color = np.expand_dims(np.array(settings.get_color(label)), axis=0)
+            # Centroids
+            color = np.array(settings.get_color(label))
             mean_x = np.mean(projected[labels == label, 0])
             mean_y = np.mean(projected[labels == label, 1])
             pyplot.scatter(mean_x, mean_y, marker='X', c=color,
                            s=150, linewidth=3, edgecolor='gray', label=f'Centroid {label}')
+            # Ellipsis
+            Views.confidence_ellipse(projected[labels == label, 0], projected[labels == label, 1], axis,
+                                      edgecolor=color, linewidth=3, zorder=0)
 
         pyplot.axis('off')
         pyplot.legend(loc='lower right')
