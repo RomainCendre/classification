000ee59eea5f59de79096b9800f79e293f84338f
Romain Cendre <romain.cendre@gmail.com>
2020-05-22 19:30:22 +0200

    Add LDA ratio function.

--- toolbox/transforms/at_most.py
+++ toolbox/transforms/at_most.py
@@ -15,6 +15,45 @@ class LDAAtMost(LinearDiscriminantAnalysis):
         return super().fit(X, y)
 
 
+# Redefinition in at most way
+class LDAAtRatio(LinearDiscriminantAnalysis):
+
+    def fit(self, X, y=None):
+        ratio = self.n_components
+        self.n_components = None
+        super().fit(X, y)
+        self._max_components = self.__select_n_components(ratio)
+        return self
+        # lda_fitted = super().fit(X, y)
+        # test = lda_fitted.__select_n_components(ratio)
+
+    def __select_n_components(self, goal_var: float) -> int:
+        var_ratio = self.explained_variance_ratio_
+
+        # Set initial variance explained so far
+        total_variance = 0.0
+
+        # Set initial number of features
+        n_components = 0
+
+        # For the explained variance of each feature:
+        for explained_variance in var_ratio:
+
+            # Add the explained variance to the total
+            total_variance += explained_variance
+
+            # Add one to the number of components
+            n_components += 1
+
+            # If we reach our goal level of explained variance
+            if total_variance >= goal_var:
+                # End the loop
+                break
+
+        # Return the number of components
+        return n_components
+
+
 class PCAAtMost(PCA):
 
     def fit_transform(self, X, y=None):
