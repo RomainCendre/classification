e0dd9691cd10319ed23984e649c90549ca549894
Romain Cendre <romain.cendre@gmail.com>
2020-06-07 10:05:41 +0200

    Update projection plot

--- toolbox/models/models.py
+++ toolbox/models/models.py
@@ -517,35 +517,6 @@ class KerasClassifier(tf.keras.wrappers.scikit_learn.KerasClassifier):
 
 
 class KerasBatchClassifier(KerasClassifier):
-    """
-    TensorFlow Keras API neural network classifier.
-
-    Workaround the tf.keras.wrappers.scikit_learn.KerasClassifier serialization
-    issue using BytesIO and HDF5 in order to enable pickle dumps.
-
-    Adapted from: https://github.com/keras-team/keras/issues/4274#issuecomment-519226139
-    """
-
-    # def __getstate__(self):
-    #     state = self.__dict__
-    #     if "model" in state:
-    #         model = state["model"]
-    #         model_hdf5_bio = io.BytesIO()
-    #         with h5py.File(model_hdf5_bio, mode="w") as file:
-    #             model.save(file)
-    #         state["model"] = model_hdf5_bio
-    #         state_copy = copy.deepcopy(state)
-    #         state["model"] = model
-    #         return state_copy
-    #     else:
-    #         return state
-    #
-    # def __setstate__(self, state):
-    #     if "model" in state:
-    #         model_hdf5_bio = state["model"]
-    #         with h5py.File(model_hdf5_bio, mode="r") as file:
-    #             state["model"] = tf.keras.models.load_model(file)
-    #     self.__dict__ = state
 
     def check_params(self, params):
         """Checks for user typos in `params`.
